a7b549f71a92da102e1de3c3c3fa88e403ff97ba
영철 신 <youngchul.shin@meshkorea.net>
2020-08-11 11:25:01 +0900

    AccountTest - 실패 코드 작성

---
+++ book_adaptivecode/SampleBank.Tests/AccountTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleBank.Tests
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void AddingTransactionChangesBalance()
+        {
+            // given
+            var account = new Account();
+
+            // when
+            account.AddTransaction(200m);
+
+            // then
+            Assert.AreEqual(200m, account.Balance);
+        }
+    }
+}
