ff1d636858fb6bb125ec44601595f0fbc45f4e6d
영철 신 <youngchul.shin@meshkorea.net>
2020-08-11 11:46:35 +0900

    AccountServiceTest - 실패 코드 작성

---
+++ book_adaptivecode/SampleBank.Tests/AccountServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleBank.Tests
+{
+    [TestClass]
+    public class AccountServiceTest
+    {
+        [TestMethod]
+        public void AddingTransactionToAccountDelegatesToAccountInstance()
+        {
+            // arrange
+            var sut = new AccountService();
+
+            // act
+            sut.AddingTransactionToAccount("예금계좌", 200m);
+
+            // assert
+            Assert.Fail();
+        }
+    }
+}
