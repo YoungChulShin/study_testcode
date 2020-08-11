44b7da896a16eba0d65ef98d98a5e48e0484c232
영철 신 <youngchul.shin@meshkorea.net>
2020-08-11 11:28:44 +0900

    AccountTest - 실패 코드 성공으로 변경

---
+++ book_adaptivecode/SampleBank/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleBank
+{
+    public class Account
+    {
+        public decimal Balance
+        {
+            get;
+            private set;
+        }
+
+        public Account()
+        {
+            Balance = 200m;
+        }
+
+        public void AddTransaction(decimal amount)
+        {
+
+        }
+    }
+}
