ea59e9396399b112c231bb43f322f9bb15515526
YoungChulShin <go1323@gmail.com>
2020-08-11 12:10:03 +0900

    AccountServiceTest - Moq 적용

--- book_adaptivecode/SampleBank.Tests/AccountServiceTest.cs
+++ book_adaptivecode/SampleBank.Tests/AccountServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using SampleBank.Tests.mock;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,9 @@ namespace SampleBank.Tests
     [TestClass]
     public class AccountServiceTest
     {
+        /*
+         * 직접 구현한 Mock 적용
+         * 
         [TestMethod]
         public void AddingTransactionToAccountDelegatesToAccountInstance()
         {
@@ -16,6 +20,23 @@ namespace SampleBank.Tests
             var account = new Account();
             var fakeRepository = new FakeAccountRepository(account);
             var sut = new AccountService(fakeRepository);
+
+            // act
+            sut.AddTransactionToAccount("예금계좌", 200m);
+
+            // assert
+            Assert.AreEqual(200m, account.Balance);
+        }
+        */
+
+        [TestMethod]
+        public void AddingTransactionToAccountDelegatesToAccountInstance()
+        {
+            // arrange
+            var account = new Account();
+            var mockRepository = new Mock<IAccountRepository>();
+            mockRepository.Setup(x => x.getByName("예금계좌")).Returns(account);
+            var sut = new AccountService(mockRepository.Object);
 
             // act
             sut.AddTransactionToAccount("예금계좌", 200m);
